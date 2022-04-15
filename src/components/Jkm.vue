e60a73e75de375134280eeae2a9c0174be92c0e5
catsgarden99 <1476125515@qq.com>
2022-04-15 11:32:20 +0800

    1

---
+++ src/components/Jkm.vue
@@ -0,0 +1,131 @@
+<template>
+    <div class="wrap">
+		<iframe v-if="hasToken" :src="iframeUrl" class="webview"></iframe>
+		<div v-else class="form">
+			<CellGroup style="margin-bottom: 100px;" inset>
+				<Field
+					v-model="tokenQrcode"
+					required
+					label="Token"
+					placeholder="请输入Token"
+				>
+				<!-- <Field
+					v-model="sms"
+					center
+					label="验证码"
+					placeholder="请输入短信验证码"
+				> -->
+					<!-- <template #button>
+						<Button :disabled="isSending" size="small" type="primary" @click="onClickSendCode">{{sendingText}}</Button>
+					</template> -->
+				</Field>
+				<Button style="margin-top: 10px;" round block type="primary" @click="onClickLogin">
+					冲冲冲
+				</Button>
+			</CellGroup>
+		</div>
+    </div>
+</template>
+
+<script setup>
+import {ref, onMounted} from 'vue'
+import { Field, CellGroup, Button, Notify } from 'vant'
+
+import { apiGetLingxiToken, apiGetSuHealthCodeToken, apiSendSmsCode } from "../apis"
+
+let hasToken = ref(false);
+let iframeUrl = ref("");
+
+let tokenQrcode = ref("");
+let sms = ref("");
+let phone = ref("");
+let isSending = ref(false);
+let cd = ref(0);
+let sendingText = ref("发送验证码");
+
+// function onClickSendCode() {
+// 	if (/^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/.test(phone.value)) {
+// 		cd.value = 60;
+// 		isSending.value = true;
+// 		sendingText.value = `${String(cd.value)}s`;
+// 		cd.value--;
+// 		let timer = setInterval(() => {
+// 			sendingText.value = `${String(cd.value)}s`;
+// 			if (cd.value <= 0) {
+// 				isSending.value = false;
+// 				sendingText.value = "发送验证码";
+// 				clearInterval(timer);
+// 			}
+// 			cd.value--;
+// 		}, 1000);
+// 		apiSendSmsCode(phone.value).then(ret => {
+// 			if (ret.data.code == 200) {
+// 				Notify({ type: 'success', message: '短信验证码发送成功' });
+// 			} else {
+// 				Notify({ type: 'warning', message: ret.data.info });
+// 			}
+// 		}).catch(err => {
+// 			console.log(err);
+// 		})
+// 	} else {
+// 		Notify({ type: 'warning', message: '请输入正确的手机号' });
+// 	}
+// }
+
+async function onClickLogin() {
+	// if (sms.value === "") {
+	// 	Notify({ type: 'warning', message: '请输入短信验证码' });
+	// 	return;
+	// }
+	// let ret = await apiGetLingxiToken(phone.value, sms.value);
+	// if (ret.data.code !== 200) {
+	// 	Notify({ type: 'warning', message: ret.data.info });
+	// 	return;
+	// }
+	// let access_token = ret.data.data.access_token;
+	// ret = await apiGetSuHealthCodeToken(access_token);
+	// if (ret.data.code !== 200) {
+	// 	Notify({ type: 'warning', message: ret.data.info });
+	// 	return;
+	// }
+	// let tokenObj = JSON.parse(String(ret.data.data).replace(/\\/g, ""));
+	// localStorage.setItem("jkm", tokenObj.token);
+
+	localStorage.setItem("jkm", tokenQrcode.value);
+	goToSuHealthCode(tokenQrcode.value);
+	hasToken.value = true;
+}
+
+
+function goToSuHealthCode(token) {
+	iframeUrl.value = "https://jsstm.jszwfw.gov.cn/jkmIndex.html?token=" + token;
+}
+
+onMounted(() => {
+    let token = localStorage.getItem("jkm");
+	if (token && token !== "") {
+		hasToken.value = true;
+		goToSuHealthCode(token);
+	}
+})
+
+</script>
+
+<style scoped>
+.wrap {
+    width: 100%;
+    height: 100%;
+}
+.wrap .webview {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+	border: medium none;
+}
+.wrap .form {
+	height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ src/components/Xcm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="wrap">
+        <iframe class="webview" src="https://xc.caict.ac.cn/#/result"></iframe>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.wrap {
+    width: 100%;
+    height: 100%;
+}
+.wrap .webview {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+	border: medium none;
+}
+</style>
