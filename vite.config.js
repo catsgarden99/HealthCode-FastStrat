bc161c1195579d8c0d2e47cb791120d2ff88d8ee
catsgarden99 <1476125515@qq.com>
2022-04-15 14:46:44 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import styleImport from 'vite-plugin-style-import'
+import  legacy  from  '@vitejs/plugin-legacy' 
 
 export default {
   plugins: [
@@ -14,5 +15,9 @@ export default {
         },
       ],
     }),
+    legacy ( { 
+      targets : [ 'defaults' ,  'not IE 11' ] 
+    } ), 
   ],
+  base:'./'
 };
